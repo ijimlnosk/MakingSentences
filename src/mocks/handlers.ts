9f891d70ef7567c8c327ac789eab9daa52a9feaf
jinsol <skek298@naver.com>
2025-02-11 22:09:35 +0900

    💡refactor: mock data 타입 오류

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,46 +3,10 @@ import { ProblemItem } from "../apis/type";
 
 const mockProblems: ProblemItem[] = [
   {
-    id: 1,
+    questionId: 1,
     title: "테스트 문제 1",
     description: "이 문장은 테스트1 문장입니다.",
     keywords: ["테스트1"],
-  },
-  {
-    id: 2,
-    title: "테스트 문제 2",
-    description: "이 문장은 테스트2 문장입니다.",
-    keywords: ["테스트2"],
-  },
-  {
-    id: 3,
-    title: "테스트 문제 3",
-    description: "이 문장은 테스트3 문장입니다.",
-    keywords: ["테스트3", "문장"],
-  },
-  {
-    id: 4,
-    title: "테스트 문제 4",
-    description: "이 문장은 테스트4 문장입니다.",
-    keywords: ["테스트4"],
-  },
-  {
-    id: 5,
-    title: "테스트 문제 5",
-    description: "오늘은 새로운 프로젝트를 시작하기 좋은 날이다.",
-    keywords: ["새로운", "좋은"],
-  },
-  {
-    id: 6,
-    title: "테스트 문제 6",
-    description: "3개월 내에 관련된 증거를 제출할 수 있다",
-    keywords: ["3개월", "증거를"],
-  },
-  {
-    id: 7,
-    title: "테스트 문제 7",
-    description: "3개월 내에 관련된 증거를 제출할 수 있다",
-    keywords: ["3개월", "증거를"],
   },
 ];
 
@@ -53,7 +17,7 @@ export const handlers = [
 
   http.get("/api/problems/:id", ({ params }) => {
     const id = params.id;
-    const problem = mockProblems.find((p) => p.id === Number(id));
+    const problem = mockProblems.find((p) => p.questionId === Number(id));
     if (problem) {
       return HttpResponse.json(problem);
     } else {
@@ -89,11 +53,11 @@ export const handlers = [
 
     const newId =
       mockProblems.length > 0
-        ? Math.max(...mockProblems.map((p) => p.id)) + 1
+        ? Math.max(...mockProblems.map((p) => p.questionId)) + 1
         : 1;
 
     const newProblem: ProblemItem = {
-      id: newId,
+      questionId: newId,
       title: body.title,
       description: body.description,
       keywords: body.keywords || [], // 없을 경우 빈 배열
